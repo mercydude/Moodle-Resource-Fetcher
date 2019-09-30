734303d42d1494218d137ad1235f7bf0c5ca44eb
norbit8 <norbit8@gmail.com>
2019-09-30 21:49:02 +0300

    fixed issues with tau

--- mrf.js
+++ mrf.js
@@ -1,8 +1,8 @@
-let btn = document.createElement("li");
-const button_html = '<div><div class="mod-indent-outer"><div class="mod-indent"></div><div><div class="activityinstance"><a id="downloaderYoav" class="" onclick="" href="" download="a"><img src="https://raw.githubusercontent.com/norbit8/Moodle-Resource-Fetcher/master/icon_download.png" class="iconlarge activityicon" alt=" " role="presentation"><span class="instancename">Download resources<span class="accesshide "> File</span></span></a> <span class="resourcelinkdetails"><font color="red">By Yoav Levy</font></span></div></div></div></div>';
+let btn = document.createElement("ul");
+const button_html = '<div><div class="mod-indent-outer"><div class="mod-indent"></div><div><div class="activityinstance"><a id="downloaderYoav" class="" onclick="" href="" download="a"><img src="https://raw.githubusercontent.com/norbit8/Moodle-Resource-Fetcher/master/icon_download.png" class="iconlarge activityicon" alt=" " role="presentation"><span class="instancename">Download resources<span class="accesshide "> File</span></span></a> <span class="resourcelinkdetails"><font color="red">By Yoav Levy</font></span><img id="loadingyoav" style="display: none;" src="https://raw.githubusercontent.com/norbit8/Moodle-Resource-Fetcher/master/buffering.gif" class="iconlarge activityicon" alt=" " role="presentation"></div></div></div></div>';
 btn.innerHTML = button_html;
 
-document.getElementById("section-0").insertBefore(btn, document.getElementById("section-0").firstChild);
+document.getElementById("region-main").insertBefore(btn, document.getElementById("region-main").firstChild);
 
 let allSections = document.getElementsByClassName('section img-text');
 let innerHtmlsList = [];
@@ -17,36 +17,53 @@ for (let i = 0; i < allSections.length; i++) {
             continue;
         }
         let strToPush = allSections[i].getElementsByTagName(keyword).item(j).innerHTML.match('href=.*?".*?"')[0];
+        let dotPDF = "";
+        if (allSections[i].getElementsByTagName(keyword).item(j).innerHTML.includes("pdf"))
+        {
+            dotPDF = ".pdf"
+        }
         if (strToPush.includes(RESOURCE)) {
-            innerHtmlsList.push(strToPush.substr(6));
+        innerHtmlsList.push([strToPush.substr(6).replace('"','').concat("&redirect=1"), allSections[i]
+            .getElementsByTagName(keyword).item(j).innerText.replace('\t\n','')
+            .split('\n')[0]+dotPDF]);
         }
     }
+}
+if (innerHtmlsList.length === 0)
+{
+    btn.hidden = true;
 }
 
 console.log(innerHtmlsList);
 
 let zip = new JSZip();
+
 document.getElementById("downloaderYoav").onclick = function (e) {
     e.preventDefault();
+    document.getElementById("loadingyoav").style.display = "";
     if (innerHtmlsList.length === 0)
     {
         alert("Error: didn't find anything to download.");
     }
+    let counter = 0;
     for (let i = 0; i < innerHtmlsList.length; i++) {
         // loading a file and add it in a zip file
-        JSZipUtils.getBinaryContent(innerHtmlsList[i], function (err, data) {
+        JSZipUtils.getBinaryContent(innerHtmlsList[i][0], function (err, data) {
             if (err) {
                 throw err; // or handle the error
             }
-            zip.file(`FileNumber_${i}`, data, {binary: true});
-            console.log(`Name${i}`);
-            if(i === (innerHtmlsList.length - 1))
+            zip.file(innerHtmlsList[i][1], data, {binary: true});
+            console.log(innerHtmlsList[i][1]);
+            counter++;
+            if (counter === (innerHtmlsList.length))
             {
                 zip.generateAsync({type: "blob"})
                     .then(function (blob) {
-                        saveAs(blob, "Resources.zip");
+                        saveAs(blob, document.title + ".zip");
                     });
+                document.getElementById("loadingyoav").style.display = "none";
             }
         });
     }
+
 };
