326f85dd71123674376d89291bc7a64eff869af8
norbit8 <norbit8@gmail.com>
2019-10-13 21:51:43 +0300

    1) Removed the use of JQuery lib. 2) Removed remote scripts. (not allowed as per the Add-on Policies) 3) Removed the minified version of JSZip library. (included only the files that are needed and used by the add-on)

--- mrf.js
+++ mrf.js
@@ -1,8 +1,25 @@
 let btn = document.createElement("ul");
 const button_html = '<div><div class="mod-indent-outer"><div class="mod-indent"></div><div><div class="activityinstance"><a id="downloaderYoav" class="" onclick="" href="" download="a"><img src="https://raw.githubusercontent.com/norbit8/Moodle-Resource-Fetcher/master/imgs/icon_download.png" class="iconlarge activityicon" alt=" " role="presentation"><span class="instancename">Download resources<span class="accesshide "> File</span></span></a> <span class="resourcelinkdetails"><font color="red">By Yoav Levy</font></span><img id="loadingyoav" style="display: none;" src="https://raw.githubusercontent.com/norbit8/Moodle-Resource-Fetcher/master/imgs/buffering.gif" class="iconlarge activityicon" alt=" " role="presentation"></div></div></div></div>';
 btn.innerHTML = button_html;
-
+btn.hidden = true;
 document.getElementById("region-main").insertBefore(btn, document.getElementById("region-main").firstChild);
+
+uls = document.getElementsByTagName("ul");
+
+let foundIt;
+for (let i = 0; i < uls.length; i++) {
+    if (uls[i].className === "topics")
+    {
+        foundIt = uls[i];
+    }
+}
+let foldersName = [];
+let sectionsArray = foundIt.childNodes;
+for(let j = 0; j < sectionsArray.length; j++){
+    foldersName.push(sectionsArray[j].getAttribute("aria-label"));
+}
+
+
 
 let allSections = document.getElementsByClassName('section img-text');
 let innerHtmlsList = [];
@@ -20,21 +37,19 @@ for (let i = 0; i < allSections.length; i++) {
         let dotPDF = "";
         if (allSections[i].getElementsByTagName(keyword).item(j).innerHTML.includes("pdf"))
         {
-            dotPDF = ".pdf"
+            dotPDF = ".pdf";
         }
         if (strToPush.includes(RESOURCE)) {
         innerHtmlsList.push([strToPush.substr(6).replace('"','').concat("&redirect=1"), allSections[i]
             .getElementsByTagName(keyword).item(j).innerText.replace('\t\n','')
-            .split('\n')[0]+dotPDF]);
+            .split('\n')[0]+dotPDF, foldersName[i]]);
         }
     }
 }
-if (innerHtmlsList.length === 0)
+if (innerHtmlsList.length !== 0)
 {
-    btn.hidden = true;
+    btn.hidden = false;
 }
-
-console.log(innerHtmlsList);
 
 let zip = new JSZip();
 
@@ -52,7 +67,7 @@ document.getElementById("downloaderYoav").onclick = function (e) {
             if (err) {
                 throw err; // or handle the error
             }
-            zip.file(innerHtmlsList[i][1], data, {binary: true});
+            zip.folder(innerHtmlsList[i][2]).file(innerHtmlsList[i][1], data, {binary: true});
             console.log(innerHtmlsList[i][1]);
             counter++;
             if (counter === (innerHtmlsList.length))
